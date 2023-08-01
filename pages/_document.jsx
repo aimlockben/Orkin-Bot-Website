516b3b36cc726c2e3aea475759734a5ce774a08f
aimlockben <141117954+aimlockben@users.noreply.github.com>
2023-08-01 12:13:28 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,14 +11,14 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="How about designing your server in the most beautiful way and entertaining the people on your server?."
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
+          <meta name="twitter:site" content="@OrkinBot" />
+          <meta name="twitter:creator" content="@OrkinBot" />
           <meta property="og:url" content="https://musicmaker.vercel.app" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="OrkinBot" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -26,14 +26,14 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="How about designing your server in the most beautiful way and entertaining the people on your server?."
           />
           <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image:alt" content="OrkinBot" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="OrkinBot"
           />
           <meta name="theme-color" content="#6108dd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
