84b7512d4e558b128f0805e6ee1ce3a1c36999d4
aimlockben <141117954+aimlockben@users.noreply.github.com>
2023-08-01 11:45:36 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -29,7 +29,7 @@ export default function Index() {
   <div className="py-10"></div>
   <Link href={"https://bit.ly/3QoDuSf"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote Astra</a>
+Vote Orkin</a>
   </Link>
         </div>
 
